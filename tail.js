d8f20eb0eac1f80894ff59f85fd5b01eabd11b7e
Aaron Bailey <aaron.bailey1618@gmail.com>
2020-01-07 04:44:27 +0000

    every element but the first function

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let slicedArray = array.slice(1);
+  return slicedArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
