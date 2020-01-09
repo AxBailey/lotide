fa7aa4cb4befbaca74121eaf11dfd194397a49e5
Aaron Bailey <aaron.bailey1618@gmail.com>
2020-01-09 01:08:02 +0000

    created function to find key

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKeyByValue = function(object, value) {
+  //changing keys to array 
+  const objectKeys = Object.keys(object) 
+
+  for(const key of objectKeys){
+    if(object[key]=== value){
+      return key;
+    }
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
